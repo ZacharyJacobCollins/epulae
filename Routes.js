5771e32a770c67415d918f9a1c0730c49144e176
Zachary Collins <zacharyjacobcollins@gmail.com>
2016-05-07 14:32:30 -0400

    working pic upload

--- Routes.js
+++ Routes.js
@@ -1,11 +1,30 @@
 var express = require('express');
-var router = express.Router();
+var multer  = require('multer');
 var LinvoDB = require("linvodb3");
+
+var router  = express.Router();
+var upload  = multer();
+//multer middleware for brevity.  Going to want to add a function that deletes old food, so hdd doesn't fill.
+var uploading = multer({
+  dest: __dirname + '../public/uploads/',
+  limits: {fileSize: 1000000, files:1},
+})
 LinvoDB.dbPath = process.cwd();
 var Foods = new LinvoDB("doc", { /* schema, can be empty */ });
 
 router.get('/',function(request,response){
     response.sendFile('Epulae-Alpha.html', {"root": __dirname});
+});
+
+router.post('/pictures/upload', [ upload.single('image'), function(request, response) {
+    var file = request.file;
+    console.log(file);
+    response.end();
+}]);
+
+router.get('/reciept',function(request,response){
+    console.log("no bueno");
+    response.sendFile('Reciept.html', {"root":__dirname + "/views/"});
 });
 
 router.get('/downloadFoodItems',function(request,response){
