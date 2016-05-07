97dc1883912ca2d6a8f855d1c9383c3a822dac12
Kisora Thomas <kisora.thomas@gmail.com>
2016-05-07 02:44:54 -0400

    Added routes in separate file

---
+++ Routes.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/',function(request,response){
+    response.sendFile('Epulae-Alpha.html', {"root": __dirname});
+});
+
+router.get('/downloadFoodItems',function(request,response){
+    console.log("New foods download request.");
+    Foods.find({}, function (err, docs) {
+        response.send(docs);
+    });
+});
+
+router.post('/addFoodItem',function(request,response){
+    console.log(request.body);
+    Foods.insert(request.body, function (err, newDoc) {
+        response.send({message:"New food item added: "+request.body.Name});
+    });
+});
+
+
+router.post('/editFoodItem',function(request,response){
+    Foods.findOne({ foodID: request.foodID }, function(err, doc) {
+        doc.save(function(err) { response.send({message:"Food updated successfully."})});
+    });
+});
+
+router.post('/deleteFoodItem',function(request,response){
+    var id = request.id;
+    Foods.remove({ id: id }, {}, function (err, numRemoved) {
+        console.log("Food removed successfully.");
+        response.send({message: "Food removed successfully: "+id})
+    });
+});
+
+modules.export = router;
